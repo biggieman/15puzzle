60e881358952c167fba3601c40f33648b1e95bf2
Aleksander <biggieman@users.noreply.github.com>
2019-01-14 21:15:56 +0300

    Updates

---
+++ precache-manifest.7dafe50baa2d095fb928505265a6f351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/15puzzle/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e7f9c7f5dcc011890b8f",
+    "url": "/15puzzle/static/js/runtime~main.e7f9c7f5.js"
+  },
+  {
+    "revision": "d7df349e8c7c5705d018",
+    "url": "/15puzzle/static/js/main.d7df349e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/15puzzle/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d7df349e8c7c5705d018",
+    "url": "/15puzzle/static/css/main.d0de1358.chunk.css"
+  },
+  {
+    "revision": "8d35f7cc1a393bd269f640c40996a657",
+    "url": "/15puzzle/index.html"
+  }
+];
